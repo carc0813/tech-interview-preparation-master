31d6557eae832924d7b07b5fa63186257741afbd
carc0813 <carc1023@hotmail.com>
2022-09-19 15:20:32 -0500

    Primer ejercicio

---
+++ 01-FindNeedle/FindNeedle.js
@@ -0,0 +1,13 @@
+function FindNeedle (haystack, needle) {
+  // Your code here:
+}
+
+
+console.log(FindNeedle('react-redux', 'redux'))//(6)
+console.log(FindNeedle('pinky', 'puntual'))//(-1)
+console.log(FindNeedle('rereredux', 'reredux'))//(2)
+
+module.exports = FindNeedle
+
+
+
