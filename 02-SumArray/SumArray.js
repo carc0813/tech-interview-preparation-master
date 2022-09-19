17469188ae0fcc15c48470972e43a4e2bf0b1942
carc0813 <carc1023@hotmail.com>
2022-09-19 16:38:51 -0500

    ejercicio 2 finalizado

--- 02-SumArray/SumArray.js
+++ 02-SumArray/SumArray.js
@@ -1,6 +1,18 @@
 function SumArray (arr, n) {
   // Your code here:
+ for(let i=0;i<arr.length;i++){
+   for(let j=i +1;j<arr.length;j++){
+    if(arr[i]+arr[j]===n){
+      return true
+    }
+   }
+ }
+ return false
+
 
 }
 
+console.log(SumArray([2, 4, 5, 9], 9))//(true)
+console.log(SumArray([2, 4, 5, 9], 12))//(false)
+console.log(SumArray([2, 5, 9], 4))//(false)
 module.exports = SumArray
