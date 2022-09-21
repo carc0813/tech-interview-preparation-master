afebc17cc534c07966d3a82123e6826ba14f3ecb
carc0813 <carc1023@hotmail.com>
2022-09-21 15:00:55 -0500

    ejercicio 4 listo

--- 04-MultidimensionalSumArray/MultidimensionalSumArray.js
+++ 04-MultidimensionalSumArray/MultidimensionalSumArray.js
@@ -1,6 +1,12 @@
-function mdArraySum (arr) {
+function mdArraySum(arr, sum = 0) {
   // Your code here:
-
+  for(const element of arr){
+    if(typeof element === "number") sum+=element
+    else sum=mdArraySum(element,sum)
+  }
+  return sum
 }
-
-module.exports = mdArraySum
+console.log(mdArraySum([1, 2, 3, 4])); //(10)
+console.log(mdArraySum([[2, 4], [1], [4, 2, 1]])); //(14)
+console.log(mdArraySum([2, [3, 4], 5, [-3, [6, [4, 5]]]])); //(26)
+module.exports = mdArraySum;
