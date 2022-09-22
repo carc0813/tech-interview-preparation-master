982122424425745b0aaf79c99b00ac7ae8413350
carc0813 <carc1023@hotmail.com>
2022-09-21 21:59:50 -0500

    Ejercicio 6 listo

--- 06-HasBalancedBrackets/HasBalancedBrackets.js
+++ 06-HasBalancedBrackets/HasBalancedBrackets.js
@@ -1,6 +1,23 @@
-function HasBalancedBrackets (string) {
+function HasBalancedBrackets(string) {
   // Your code here:
+  let stack = [];
+  const validateBrakets = {
+    "{": "}",
+    "[": "]",
+    "(": ")",
+  };
+  for (const bracket of string) {
+    if (validateBrakets[bracket]) stack.push(bracket);
 
+    else if (validateBrakets[stack.pop()] !== bracket) return false;
+  }
+
+  return !stack.length;
 }
 
-module.exports = HasBalancedBrackets
+console.log(HasBalancedBrackets("{[]()}")); //(true)
+console.log(HasBalancedBrackets("{[(])}")); //(false)
+console.log(HasBalancedBrackets("{[(")); //(false)
+console.log(HasBalancedBrackets("{[([{()[]{}}])]}")); //(true)
+console.log(HasBalancedBrackets("{[]}}")); //(false)
+module.exports = HasBalancedBrackets;
